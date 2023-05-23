e1c737134c49fc9de484f3a102b80e77f564c9d6
Gihan Duane <duanegaid.102094@gmail.com>
2023-05-23 19:12:34 +0800

    change the edit syntax, did not remove  old code

--- src/components/buttons/Edit.jsx
+++ src/components/buttons/Edit.jsx
@@ -1,5 +1,5 @@
 const Edit = () => {
-  return <button className="btn btn-primary btn-sm">Edit</button>;
+  return <button className="btn btn-warning m-2 btn-sm" data-bs-toggle="modal" data-bs-target="#form">Edit</button>;
 };
 
 export default Edit;
